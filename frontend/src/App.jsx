341effc76537798b22c3a1435dc36d58a8ddf53d
Jonast96 <Jon.Astveit@hotmail.com>
2024-03-05 01:57:41 +0100

    added basic frontend for testing adding notes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState({
+    title: "",
+    content: "",
+  });
 
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log(notes);
+    setNotes({
+      title: "",
+      content: "",
+    });
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Note taker</h1>
+      <p>Add Note</p>
+
+      <form action="submit">
+        <input
+          onChange={(e) => {
+            setNotes({
+              ...notes,
+              title: e.target.value,
+            });
+          }}
+          placeholder="Title"
+          type="text"
+          name="title"
+          id="title"
+          value={notes.title}
+        />
+        <input
+          onChange={(e) => {
+            setNotes({
+              ...notes,
+              content: e.target.value,
+            });
+          }}
+          placeholder="Content"
+          type="text"
+          name="content"
+          id="content"
+          value={notes.content}
+        />
+        <input type="button" value="Submit" onClick={onSubmit} />
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
